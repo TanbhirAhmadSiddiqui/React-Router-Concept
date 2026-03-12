f92b7ad859b7be7feca7b5c108af35d9cfad156a
Tanbhir Ahmad <tanbhirahmadsiddiqui@gmail.com>
2026-03-12 12:45:50 +0600

    Use navLink and React Loader

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,25 @@ import App from "./App.jsx";
 // react router
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from "./Components/Root/Root.jsx";
+import About from "./Components/About/About.jsx";
+import Blogs from "./Components/Blogs/Blogs.jsx";
 
 // create router
 const router = createBrowserRouter([
   {
     path: "/",
     // element: <div>Hello World</div>,
-    Component: App,
+    Component: Root,
+    children: [
+      { index: true, Component: App },
+      { path: "/about", Component: About },
+      {
+        path: "/blogs",
+        loader: () => fetch("https://jsonplaceholder.typicode.com/posts"),
+        Component: Blogs,
+      },
+    ],
   },
 ]);
 createRoot(document.getElementById("root")).render(
